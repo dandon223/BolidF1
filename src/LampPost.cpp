f8e2b220b43edf7f9c76e45e710bbe30bcaca0c7
Konrad <konrad.bratosiewicz@gmail.com>
2021-01-27 23:24:49 +0100

    Added lamp post bar

--- src/LampPost.cpp
+++ src/LampPost.cpp
@@ -8,10 +8,18 @@
 LampPost::LampPost(const ShaderProgram *basic_shader, const ShaderProgram *light_shader, std::vector<LightSource*>& point_lights):
 	Model(glm::vec3(-8.0, 0.0, 0.0), glm::vec3(1.0, 1.0, 1.0))
 {
-	Object3D* post_base = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.1, 3.0, 0.1), basic_shader);
-	post_base->translate(glm::vec3(0.0, 2.0, 0.0));
+	Object3D* post_base = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.1, 5.0, 0.1), basic_shader);
+	post_base->translate(glm::vec3(0.0, 4.0, 0.0));
 	post_base->set_geometry(CUBE_VERTICES, CUBE_INDICES);
 	post_base->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/bricks.png"));
 	add(post_base);
+
+	Object3D* post_bar = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.07, 1.3, 0.07), basic_shader);
+	post_bar->rotate(-80.0, glm::vec3(0.0, 0.0, 1.0));
+	post_bar->translate(glm::vec3(1.0, 9.0, 0.0));
+	post_bar->set_geometry(CUBE_VERTICES, CUBE_INDICES);
+	post_bar->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/bricks.png"));
+	add(post_bar);
+
 	bind_buffers();
 }
