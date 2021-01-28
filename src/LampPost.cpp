6f379235fb4f8b36ab2a1191ee4842fbf5c09962
Konrad <konrad.bratosiewicz@gmail.com>
2021-01-28 17:33:56 +0100

    Reduced number of texture loads

--- src/LampPost.cpp
+++ src/LampPost.cpp
@@ -8,24 +8,25 @@
 LampPost::LampPost(const ShaderProgram *basic_shader, const ShaderProgram *light_shader, std::vector<LightSource*>& point_lights):
 	Model(glm::vec3(-8.0, 0.0, 0.0), glm::vec3(1.0, 1.0, 1.0))
 {
+	GLuint texture_id = LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/lamp_post.png");
 	Object3D* base = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.1, 5.0, 0.1), basic_shader);
 	base->translate(glm::vec3(0.0, 4.0, 0.0));
 	base->set_geometry(CUBE_VERTICES, CUBE_INDICES);
-	base->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/lamp_post.png"));
+	base->set_texture(texture_id);
 	add(base);
 
 	Object3D* bar = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.07, 1.3, 0.07), basic_shader);
 	bar->rotate(-80.0, glm::vec3(0.0, 0.0, 1.0));
 	bar->translate(glm::vec3(1.0, 9.0, 0.0));
 	bar->set_geometry(CUBE_VERTICES, CUBE_INDICES);
-	bar->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/lamp_post.png"));
+	bar->set_texture(texture_id);
 	add(bar);
 
 	Object3D* light_case = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.1, 0.5, 0.25), basic_shader);
 	light_case->rotate(-80.0, glm::vec3(0.0, 0.0, 1.0));
 	light_case->translate(glm::vec3(2.0, 9.18, 0.0));
 	light_case->set_geometry(CUBE_VERTICES, CUBE_INDICES);
-	light_case->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/lamp_post.png"));
+	light_case->set_texture(texture_id);
 	add(light_case);
 
 	light_ = create_pointLight(glm::vec3(0.0, 0.0, 0.0), glm::vec3(1.0, 1.0, 1.0), 0.2, 1.0, 1.0, 1.0, 0.009, 0.0032, *light_shader, point_lights);
