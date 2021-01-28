df60e02a859f322ab600aa1bb5c50a011110d260
Konrad <konrad.bratosiewicz@gmail.com>
2021-01-28 17:20:11 +0100

    WIP lamp texture

--- src/LampPost.cpp
+++ src/LampPost.cpp
@@ -11,21 +11,21 @@ LampPost::LampPost(const ShaderProgram *basic_shader, const ShaderProgram *light
 	Object3D* base = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.1, 5.0, 0.1), basic_shader);
 	base->translate(glm::vec3(0.0, 4.0, 0.0));
 	base->set_geometry(CUBE_VERTICES, CUBE_INDICES);
-	base->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/bricks.png"));
+	base->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/lamp_post.png"));
 	add(base);
 
 	Object3D* bar = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.07, 1.3, 0.07), basic_shader);
 	bar->rotate(-80.0, glm::vec3(0.0, 0.0, 1.0));
 	bar->translate(glm::vec3(1.0, 9.0, 0.0));
 	bar->set_geometry(CUBE_VERTICES, CUBE_INDICES);
-	bar->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/bricks.png"));
+	bar->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/lamp_post.png"));
 	add(bar);
 
 	Object3D* light_case = new Object3D(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.1, 0.5, 0.25), basic_shader);
 	light_case->rotate(-80.0, glm::vec3(0.0, 0.0, 1.0));
 	light_case->translate(glm::vec3(2.0, 9.18, 0.0));
 	light_case->set_geometry(CUBE_VERTICES, CUBE_INDICES);
-	light_case->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/bricks.png"));
+	light_case->set_texture(LoadMipmapTexture(GL_TEXTURE0, "../ResourceFiles/lamp_post.png"));
 	add(light_case);
 
 	light_ = create_pointLight(glm::vec3(0.0, 0.0, 0.0), glm::vec3(1.0, 1.0, 1.0), 0.2, 1.0, 1.0, 1.0, 0.009, 0.0032, *light_shader, point_lights);
